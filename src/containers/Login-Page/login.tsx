20a8bb0a8634ead14540c4f72869c249dc866dd7
jauafelipe <jauafelipe345@gmail.com>
2023-04-10 03:37:43 -0300

    test

--- src/containers/Login-Page/login.tsx
+++ src/containers/Login-Page/login.tsx
@@ -2,22 +2,12 @@ import * as S from './style';
 import { Login } from '@/interface/login-props';
 import { ButtonComp } from '@/components/button/button';
 import { AiOutlineUser } from 'react-icons/ai';
+
 export default function LoginPainel({ titleLogin }: Login) {
   return (
     <S.Container>
       <AiOutlineUser className="icon-login" />
-      <h2>{titleLogin}</h2>
-      <form action="">
-        <input type="email" name="" id="" />
-        <input type="password" />
-        <div className="remember-me">
-          <input type="checkbox" name="" id="" />
-          <p>Manter-me Conectado</p>
-        </div>
-        <ButtonComp text={'LOGAR'} />
-      </form>
-      <p>Não Tem Uma Conta?</p>
-      <a>Criar Conta</a>
+
     </S.Container>
   );
 }
